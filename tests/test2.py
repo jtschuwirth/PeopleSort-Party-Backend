50ec3dc55ad6ff48c3cb6a202f3b8419ac63a7a1
jtschuwirth <jtschuwirth@uc.cl>
2022-10-22 17:12:57 -0300

    changed get all recipients to work with rooms

--- tests/test2.py
+++
@@ -1,7 +0,0 @@
-import requests
-import random
-
-level=random.randint(1,3)
-prompt = requests.get(url=f"https://59fxcxkow4.execute-api.us-east-1.amazonaws.com/dev/nuncanunca/phrases?level={level}")
-
-print(prompt.json()[0]["phrase"])
